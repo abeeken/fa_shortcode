e1404a55bedb45344d9924e0021c8eaffd6f491b
Andrew Beeken <abeeken@gmail.com>
2020-01-13 11:07:27 +0000

    Create fa_shortcode.php

---
+++ fa_shortcode.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+     * FontAwesome Shortcode
+     * 
+     * Useage: [fa fa_code=fa-bullhorn.fa-lg]
+     * 
+     * Use . to combine FontAwesome attributes - shortcode inserts <i> tag with required classes
+     */
+    add_shortcode('fa', 'fa_shortcode');
+
+    function fa_shortcode( $atts ){
+        extract( shortcode_atts(
+                array(
+                    'fa_code' => '',
+                ), $atts )
+        );
+        $fa_code = str_replace('.',' ',$fa_code);
+        return '<i class="fa '.$fa_code.'"></i>';
+    }
+?>
